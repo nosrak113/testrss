0cb442a8d3bef339512205b417c05e2b3eeba34d
Karson <nosrak113@gmail.com>
2015-11-03 13:26:01 -0800

    add pods

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '9.0'
+use_frameworks!
+inhibit_all_warnings!
+
+target 'rss' do
+
+pod 'Realm'
+pod 'RealmSwift'
+pod 'Alamofire'
+
+end
+
---
+++ rss/Controllers/rssViewController.swift
@@ -0,0 +1,13 @@
+//
+//  rssViewController.swift
+//  rss
+//
+//  Created by Karson Miller on 11/3/15.
+//  Copyright © 2015 nack. All rights reserved.
+//
+
+import UIKit
+
+class rssViewController: UIViewController {
+
+}
